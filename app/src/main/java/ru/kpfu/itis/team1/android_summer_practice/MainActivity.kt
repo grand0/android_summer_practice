7fcba391a749ed1250c31336793c0b4b2a43a368
Ilya Ponomarev <iponom0420@gmail.com>
2023-06-29 13:19:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/kpfu/itis/team1/android_summer_practice/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.kpfu.itis.team1.android_summer_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
